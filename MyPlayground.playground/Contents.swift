147e24ee71768af46e49e2ded52956641d49d476
ket <ketvitketvit@gmail.com>
2018-12-16 09:23:52 +0200

    Функция прининимающая [String] и возвращающая отсортированный ->[Word].

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -19,7 +19,6 @@ import Foundation
  dog: 1
  
  Proper error checking/exception handling is not required.
- 
  */
 
 struct Word: Comparable, CustomStringConvertible
@@ -51,17 +50,50 @@ struct Word: Comparable, CustomStringConvertible
     var description: String {
         return "\(self.string): \(self.frequency)"
     }
-
 }
 
 func words() -> [String] {
-    let input = "I am already far north of London and as I walk in the streets of Petersburgh I feel a cold northern breeze play upon my cheeks which braces my nerves and fills me with delight Do you understand this feeling This breeze which has travelled from the regions towards which I am advancing gives me a foretaste of those icy climes Inspirited by this wind of promise my daydreams become more fervent and vivid I try in vain to be persuaded that the pole is the seat of frost and desolation it ever presents itself to my imagination as the region of beauty and delight There Margaret the sun is forever visible its broad disk just skirting the horizon and diffusing a perpetual splendour Therefore with your leave my sister I will put some trust in preceding navigators there snow and frost are banished and sailing over a calm sea we may be wafted to a land surpassing in wonders and in beauty every region hitherto discovered on the habitable globe Its productions and features may be without example as the phenomena of the heavenly bodies undoubtedly are in those undiscovered solitudes What may not be expected in a country of eternal light I may there discover the wondrous power which attracts the needle and may regulate a thousand celestial observations that require only this voyage to render their seeming eccentricities consistent forever I shall satiate my ardent curiosity with the sight of a part of the world never before visited and may tread a land never before imprinted by the foot of man These are my enticements and they are sufficient to conquer all fear of danger or death and to induce me to commence this laborious voyage with the joy a child feels when he embarks in a little boat with his holiday mates on an expedition of discovery up his native river But supposing all these conjectures to be false you cannot contest the inestimable benefit which I shall confer on all mankind to the last generation by discovering a passage near the pole to those countries to reach which at present so many months are requisite or by ascertaining the secret of the magnet which if at all possible can only be effected by an undertaking such as mine"
+    let input = "I am already far north of London and as I walk in the streets of Petersburgh I feel a cold northern breeze play upon my cheeks which braces my nerves and fills me with delight Do you understand this feeling This breeze which has travelled from the regions towards which I am advancing gives me a foretaste of those icy climes Inspirited by this wind of promise my daydreams become more fervent and vivid I try in vain to be persuaded that the pole is the seat of frost and desolation it ever presents itself to my imagination as the region of beauty and delight There Margaret the sun is forever visible its broad disk just skirting the horizon and diffusing a perpetual splendour Therefore with your leave my sister I will put some trust in preceding navigators there snow and frost are banished and sailing over a calm sea we may be wafted to a land surpassing in wonders and in beauty every region hitherto discovered on the habitable globe Its productions and features may be without example as the phenomena of the heavenly bodies undoubtedly are in those undiscovered solitudes What may not be expected in a country of eternal light I may there discover the wondrous power which attracts the needle and may regulate a thousand celestial observations that require only this voyage to render their seeming eccentricities consistent forever I shall satiate my ardent curiosity with the sight of a part of the world never before visited and may tread a land never before imprinted by the foot of man These are my enticements and they are sufficient to conquer all fear of danger or death and to induce me to commence this laborious voyage with the joy a child feels when he embarks in a little boat with his holiday mates on an expedition of discovery up his native river But supposing all these conjectures to be false you cannot contest the inestimable benefit which I shall confer on all mankind to the last  by discovering a passage near the pole to those countries to reach which at present so many months are requisite or by ascertaining the secret of the magnet which if at all possible can only be effected by an undertaking such as mine"
     return input.lowercased().components(separatedBy: .whitespacesAndNewlines)
 }
+//let inputWords = words()
 
-let inputWords = words()
+
 var outputObjects = [Word]()
+// Функция, которая принимает массив строк и возвращает массив структур отсортированных по frequency и string.
+func converter(for inputArray: [String]) -> [Word] {
+    for word in Set(inputArray) {
+        outputObjects.append(Word(string: word, frequency: countFrequency(the: word, from: inputArray)))
+    }
+    outputObjects.sort(by: {sortByFrequencyAndAlphabet($0, $1)})
+    return outputObjects
+}
+// Функция, которая считает сколько раз встречается заданое значение в массиве.
+func countFrequency(the word: String,from array: [String] ) -> Int {
+    var frequency = 0
+    for element in array where element == word {
+        frequency += 1
+    }
+    return frequency
+}
+// Функция, которая сортирует масив по убыванию frequency, а вслучае равенаства по алфавиту.
+func sortByFrequencyAndAlphabet(_ lhs: Word, _ rhs: Word) -> Bool {
+    if lhs.frequency == rhs.frequency {
+        if lhs.string < rhs.string {
+            return true
+        } else {
+            return false
+        }
+    } else if lhs.frequency > rhs.frequency {
+        return true
+    } else {
+        return false
+    }
+}
+// Вызов первой функции. :D
+print(converter(for: words()))
 
+/*
 func searchMatched(for word: String) -> Bool{
     for (index,object) in outputObjects.enumerated() {
         if object.string == word{
@@ -79,64 +111,10 @@ for word in inputWords{
     }
 }
 
-func sortByAlphabet(_ lhs: Word, _ rhs: Word) -> Bool {
-    if lhs.frequency == rhs.frequency {
-        if lhs.string < rhs.string {
-            return true
-        } else {
-            return false
-        }
-    } else if lhs.frequency > rhs.frequency {
-        return true
-    } else {
-        return false
-    }
-}
 // 1. Count the unique words in array.
 outputObjects.count
-// 2. Sort the words by descending frequency. && Sort alphabetically.
-outputObjects.sort(by: {sortByAlphabet($0, $1)})
+// 2. Sort the words by descending frequency. && 4. Sort alphabetically.
+outputObjects.sort(by: {sortByFrequencyAndAlphabet($0, $1)})
 // 3. Then display the result to standard out.
 outputObjects.forEach({print($0)})
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// 1) variant
-let testString = "Hit the road Jack, Jack on road back jach jach jack jack"
-let smallTestString = testString.replacingOccurrences(of: ",", with: "")
-// create array from string
-var wordsArray = smallTestString.lowercased().split(separator: " ")
-var keyValueStore = [String: Int]()
-// from array to dictionary
-for word in wordsArray {
-    keyValueStore.updateValue(0, forKey: String(word))
-}
-// compared word of array to dictionary key(String), if it equal - value(Int) incremented.
-for word in wordsArray {
-    for (key, value) in keyValueStore {
-        if key == word {
-            keyValueStore.updateValue(value + 1, forKey: key)
-        }
-    }
-}
-// sorted dictionary by descending values
-var newDictStore = keyValueStore.sorted { $0.value > $1.value }
-// output to std_out
-for (dictKeyWord, equals) in newDictStore
-{
-    for wordFromArray in wordsArray
-    {
-        if dictKeyWord == wordFromArray{
-            print(dictKeyWord + ":", equals)
-            wordsArray.removeAll(where: { $0 == wordFromArray })
-
-            //wordsArray.filter({$0 != wordFromArray})
-            //wordsArray.removeAtIndex(wordsArray.indexOf(wordFromArray))
-        }
-    }
-}
-
-
-
-
+*/
